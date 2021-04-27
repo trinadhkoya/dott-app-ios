8ff78b82cc667c289c7e823a0a8907c40be08653
Trinadh Koya <trinadhkoya@Trinadhs-MacBook-Pro.local>
2021-04-27 12:34:25 +0530

    first commit

---
+++ Get Started/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get Started
+//
+//  Created by Trinadh Koya on 07/02/1943 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
